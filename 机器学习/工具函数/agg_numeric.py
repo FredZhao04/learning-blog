bc6b596a785119bdd9e096f800cb7fa95f012f66
zhaoxining <zhaoxining@higinet.com.cn>
2020-03-11 22:38:52 +0800

    消防知识总结
    消防知识总结

---
+++ 机器学习/工具函数/agg_numeric.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+def agg_numeric(df, group_var, df_name):
+    """Aggregates the numeric values in a dataframe. This can
+    be used to create features for each instance of the grouping variable.
+
+    Parameters
+    --------
+        df (dataframe):
+            the dataframe to calculate the statistics on
+        group_var (string):
+            the variable by which to group df
+        df_name (string):
+            the variable used to rename the columns
+
+    Return
+    --------
+        agg (dataframe):
+            a dataframe with the statistics aggregated for
+            all numeric columns. Each instance of the grouping variable will have
+            the statistics (mean, min, max, sum; currently supported) calculated.
+            The columns are also renamed to keep track of features created.
+
+    """
+    # Remove id variables other than grouping variable
+    for col in df:
+        if col != group_var and 'SK_ID' in col:
+            df = df.drop(columns=col)
+
+    group_ids = df[group_var]
+    numeric_df = df.select_dtypes('number')
+    numeric_df[group_var] = group_ids
+
+    # Group by the specified variable and calculate the statistics
+    agg = numeric_df.groupby(group_var).agg(['count', 'mean', 'max', 'min', 'sum']).reset_index()
+
+    # Need to create new column names
+    columns = [group_var]
+
+    # Iterate through the variables names
+    for var in agg.columns.levels[0]:
+        # Skip the grouping variable
+        if var != group_var:
+            # Iterate through the stat names
+            for stat in agg.columns.levels[1][:-1]:
+                # Make a new column name for the variable and stat
+                columns.append('%s_%s_%s' % (df_name, var, stat))
+
+    agg.columns = columns
+    return agg
